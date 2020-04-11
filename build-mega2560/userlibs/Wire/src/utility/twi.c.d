056d1860e6fb4304e6f95539af53ffdb562364bd
Weichen <w3ichen@gmail.com>
2020-04-10 20:57:00 -0600

    zipped file

--- build-mega2560/userlibs/Wire/src/utility/twi.c.d
+++
@@ -1,6 +0,0 @@
-build-mega2560/userlibs/Wire/src/utility/twi.c.o: \
- /home/cmput274/arduino-ua/libraries/Wire/src/utility/twi.c \
- /usr/share/arduino/hardware/arduino//cores/arduino/Arduino.h \
- /usr/share/arduino/hardware/arduino//cores/arduino/binary.h \
- /usr/share/arduino/hardware/arduino//variants/mega/pins_arduino.h \
- /home/cmput274/arduino-ua/libraries/Wire/src/utility/twi.h
